b95a5a864c08277c435353ba401a03cca1a88537
Aytursun Nur <aytursunnur@Aytursuns-MacBook-Pro.local>
2023-06-22 12:23:53 +1000

    testcases

--- src/test/java/registrationFunctionality/registerInvalidCredentials.java
+++ src/test/java/registrationFunctionality/registerInvalidCredentials.java
@@ -4,6 +4,14 @@ import utilities.commonMethods;
 
 public class registerInvalidCredentials extends commonMethods{
 	
-	//delete me
+public void test2() {
+	rp.myaccount.click();
+	rp.register.click();
+	//test automation stopped as the bug found in step 4 as 
+	//registration successful with combination of letter and 
+	//numbers in Telephone field. bug report has been done. 
+	//awaiting developer to fix the problem.
+	
+}
 
 }
--- src/test/java/registrationFunctionality/successfulRegistration.java
+++ src/test/java/registrationFunctionality/successfulRegistration.java
@@ -1,9 +1,39 @@
 package registrationFunctionality;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import utilities.commonMethods;
+import utilities.configReader;
 
 public class successfulRegistration extends commonMethods{
+	@Test
+
 	
-	//delete me
+	public void test1() {
+		
+		
+		rp.myaccount.click();
+		
+		rp.register.click();
+		
+		
+		rp.firstname.sendKeys(configReader.getProperty("firstname"));
+		rp.lastname.sendKeys(configReader.getProperty("lastname"));
+	    rp.email.sendKeys(configReader.getProperty("E-mail"));
+	    rp.telephone.sendKeys(configReader.getProperty("Telephone"));
+	    rp.password.sendKeys(configReader.getProperty("password"));
+	    rp.confirmpassword.sendKeys(configReader.getProperty("confirmPassword"));
+	    
+	    rp.privacypolicy.click();
+	    rp.checkbox.click();
+	    rp.continueButton.click();
+	    
+	    Assert.assertTrue(rp.successfullMessage.isDisplayed());
+	    
+	}
+	
+	
+	
 
 }
