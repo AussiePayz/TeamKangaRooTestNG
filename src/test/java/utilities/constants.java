3b5a67ae8aed80c4c4a40ca00126c40de259b5ca
sheerin <sheerintursun@sheerins-MacBook-Pro.local>
2023-06-23 22:13:14 +1000

    sheerin commit

--- src/test/java/utilities/constants.java
+++ src/test/java/utilities/constants.java
@@ -5,6 +5,6 @@ public class constants {
 	public static final int implicit_wait_time = 10;
     public static final int explicit_wait_time = 30;
     
-    public static final String configProperty_filePath = "src\\test\\resources\\propertiesFolder\\configuration.properties";
+    public static final String configProperty_filePath = "src/test/resources/propertiesFolder/configuration.properties";
 
 }
