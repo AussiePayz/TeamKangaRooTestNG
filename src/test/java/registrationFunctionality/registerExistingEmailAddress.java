7e5a31b1c0990deb40c28f8cb08141c89727f656
Aytursun Nur <aytursunnur@aytursuns-mbp.home>
2023-06-25 21:52:49 +1000

    testcases

--- src/test/java/registrationFunctionality/registerExistingEmailAddress.java
+++ src/test/java/registrationFunctionality/registerExistingEmailAddress.java
@@ -1,9 +1,29 @@
 package registrationFunctionality;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import utilities.commonMethods;
+import utilities.configReader;
 
 public class registerExistingEmailAddress extends commonMethods {
 	
-	//delete me
+	@Test
+	public void testcase3() {
+		rp.myaccount.click();
+		rp.register.click();
+		Assert.assertTrue(rp.RegisterAccountPage.isDisplayed());
+		rp.firstname.sendKeys(configReader.getProperty("firstname"));
+		rp.lastname.sendKeys(configReader.getProperty("lastname"));
+	    rp.email.sendKeys(configReader.getProperty("E-mail"));
+	    rp.telephone.sendKeys(configReader.getProperty("Telephone"));
+	    rp.password.sendKeys(configReader.getProperty("password"));
+	    rp.confirmpassword.sendKeys(configReader.getProperty("confirmPassword"));
+	    
+	    rp.privacypolicy.click();
+	    rp.checkbox.click();
+	    rp.continueButton.click();
+	    Assert.assertTrue(rp.emailExistingMessage.isDisplayed());
+	}
 
 }
