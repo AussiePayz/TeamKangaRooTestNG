dfd5d4aae1a2781151ae2389ccdf80bb5862ec37
Aradnuyan2 <sadeghi.arezoo19@gmail.com>
2023-06-27 18:29:50 -0400

    Created 4 test cases and deleted 1 in searchBarFunctionality. Made changes to searchBar page and config properties

--- src/test/java/pages/searchBarPage.java
+++ src/test/java/pages/searchBarPage.java
@@ -1,14 +1,35 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import utilities.commonMethods;
 import utilities.driver;
 
-public class searchBarPage {
+public class searchBarPage  extends commonMethods {
 	
 	public searchBarPage() {
 		
 		PageFactory.initElements(driver.getDriver(), this);
+		
 	}
+	@FindBy(xpath = "//*[@title='My Account']")
+	public WebElement MyAccount;
+		
+	@FindBy(xpath = "//input[@name='search']")
+	public WebElement searchBar;	
+		
+	@FindBy(xpath = "//button[@class='btn btn-default btn-lg']")
+	public WebElement searchButton;	
+		
+	@FindBy(xpath = "//div[@class='product-thumb']/div/a")
+	public WebElement firstItemThumbnail;
+	
+	@FindBy(xpath = "//button[@id='button-cart']")
+	public WebElement addToCartButton;
+	@FindBy(xpath = "//div[@id='content']/p[2]")
+	public WebElement pageMessage;
+			
+		}
 
-}
